7e2b433e110347a141d1a8200a348aaca7342526
Ivy Liu <96558257+waterRK9@users.noreply.github.com>
2022-11-13 14:37:41 -0500

    Squashed commit of the following:
    commit d7fdee2a18a1d23bcff7ae0091625cd0264dd4f4
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Sun Nov 13 14:37:15 2022 -0500
    
        replaced test values with actual wires
    
    commit 4ed8827a5a0dafcac3f2d6fd5de21646f5ba4178
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Sun Nov 13 14:25:52 2022 -0500
    
        refactored eth_packer to comb and seq
    
    commit 317e2c7cebfa2bdf58c9165f256ef762be2ebc1a
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Sun Nov 6 14:43:36 2022 -0500
    
        Update eth_packer_tb.sv
    
    commit bcb30b283f66b10e51fe96ae53dd5fe3523a93db
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Sun Nov 6 14:36:14 2022 -0500
    
        ethernet_packager -> eth_packer
    
    commit d485bc83b0f707852cf747baf0b62d1e870df4aa
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Sun Nov 6 14:34:01 2022 -0500
    
        drafted ethernet_packager
    
    commit 6ff90f9568a05dc2242b0b3d7865a182b9abefd7
    Merge: 292dded f6352ac
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Sun Nov 6 10:35:47 2022 -0500
    
        Merge branch 'main' into ethernet-fun
    
    commit 292ddeddf3763f68c71080e73c0cf148720ecaf7
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Sun Nov 6 10:35:06 2022 -0500
    
        trying to fix whatever merge issue is happening
    
    commit 962adc8ae2595a68cb3e17ae38f159f232843416
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Thu Nov 3 20:25:51 2022 -0400
    
        Update ethernet_packager.sv
    
    commit 70b0402f7313f4ce8c6a38cd563bfffc3ccb38ed
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Thu Nov 3 13:55:46 2022 -0400
    
        created ethernet_packager.sv skeleton
    
    commit 5f7f14ba7e5570ef78c508966c8948e5b4103c76
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Thu Nov 3 13:19:29 2022 -0400
    
        fixed pixel byte dibit counting issue
    
    commit 7501c40ec15e1e9222fe9b8a2860786ba236fdcb
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Thu Nov 3 13:03:46 2022 -0400
    
        Update reverse_bit_order_tb.sv
    
    commit 1389670e1433fd53ac5d839eb53b1ecd3eebaf44
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Tue Nov 1 23:08:46 2022 -0400
    
        added printout to testbench
    
    commit 4cfcf699197678502e2f624eab68c24398889890
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Tue Nov 1 22:35:50 2022 -0400
    
        tweaking test
    
    commit 0795a2bcb9e42541965e86ce604c240655033277
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Tue Nov 1 22:10:32 2022 -0400
    
        inital positive response to testbench
    
    commit f5f40b905bf6b8f03a067ff1b8072e805216a61e
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Tue Nov 1 22:03:33 2022 -0400
    
        reverse bit order testbench attempt
    
    commit d5806b97fcb84e6e5833c0889cb31a3f046638d3
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Tue Nov 1 21:37:27 2022 -0400
    
        Delete Untitled.rtf
    
    commit 6cbd4b4af0bac2b8869010e960469187b542bf10
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Tue Nov 1 21:34:21 2022 -0400
    
        reverse_bit_order
    
    commit 2e415e95a91b67122e1b9dbb0a72acd08e7e4487
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Tue Nov 1 21:05:53 2022 -0400
    
        setup in toplevel
    
    commit e6ae44c1dbf297d808a777e73836c1915eef792d
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Tue Nov 1 15:04:26 2022 -0400
    
        adding in xilinx dual port ram
    
    commit 0b2631e2bb425e748fd8f80ce5fea618024e52d2
    Author: Ivy Liu <96558257+waterRK9@users.noreply.github.com>
    Date:   Mon Oct 31 23:16:18 2022 -0400
    
        initial stab at bit_order_reverse

--- FPGA1/src/eth_packer.sv
+++ FPGA1/src/eth_packer.sv
@@ -1,7 +1,7 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-module ethernet_packager (
+module eth_packer (
     input wire clk,
     input wire rst,
     input wire axiiv,
@@ -15,11 +15,12 @@ module ethernet_packager (
 parameter DEST_ADDR_DIBIT = 2'b11; 
 parameter SOURCE_ADDR = 48'h69695A065491; //note: flip to MSB/ LSb order if using
 
-parameter PREAMBLE_DIBITS = 32;
-parameter ADDR_DIBITS = 24;
-parameter MIN_DATA_DIBITS = 320 * 4;
-parameter CRC_DIBITS = 16;
-parameter IFG_PERIOD = 48; // Interpacket-Gap: standard minimum is time to send 96 bits (43 cycles)
+parameter PREAMBLE_DIBITS = 32 - 1;
+parameter ADDR_DIBITS = 24 - 1;
+parameter MIN_DATA_DIBITS = 20 - 1; //(320 * 4) - 1;
+parameter CRC_DIBITS = 16 - 1;
+parameter IFG_PERIOD = 48 -1; // Interpacket-Gap: standard minimum is time to send 96 bits (43 cycles)
+parameter LEN_DIBITS = 8 - 1;
 
 logic [3:0] state;
 
@@ -28,125 +29,169 @@ logic [2:0] byte_bit_counter;
 logic [12:0] dibit_counter;
 logic [8:0] audio_counter;
 
-typedef enum {Idle, SendPre, SendDestAddr, SendSourceAddr, SendData, SendTail} States;
+typedef enum {Idle = 0,
+              SendPre = 1, 
+              SendDestAddr = 2, 
+              SendSourceAddr = 3, 
+              SendLength = 4,
+              SendData = 5, 
+              SendTail = 6} States;
 
 // All modules (& associated logic) here
+logic crc32rst;
 logic cksum_axiiv;
 logic [31:0] cksum;
 logic cksum_calculated;
 crc32 crc32 (
     .clk(clk),
-    .rst(rst),
+    .rst(crc32rst),
     .axiiv(cksum_axiiv), //note: want to omit preamble, and sfd
     .axiid(phy_txd), 
     .axiov(cksum_calculated),
     .axiod(cksum)
 );
+always_comb begin
+    if (state > 1 && state < 6) begin
+        cksum_axiiv = 1;
+    end else begin
+        cksum_axiiv = 0;
+    end
+
+    case(state)
+        Idle:begin
+            phy_txen = 1'b0;
+            phy_txd = 2'b0;
+            stall = 1;
+        end
+        SendPre: begin
+            phy_txen = 1'b1;
+            if (dibit_counter < PREAMBLE_DIBITS) phy_txd = 2'b01; 
+            else if (dibit_counter == PREAMBLE_DIBITS) phy_txd = 2'b11; 
+            stall = 1;
+        end
+        SendDestAddr: begin
+            phy_txen = 1'b1;
+            phy_txd = 2'b11; //broadcast all
+            stall = 1;
+        end
+        SendSourceAddr: begin
+            phy_txen = 1'b1;
+            phy_txd = 2'b10; //hardcode to random source address, FPGA2 doesn't care
+            stall = 1;
+        end
+        SendLength: begin
+            phy_txen = 1'b1;
+            phy_txd = 2'b01; //note: FPGA2 doesn't care about length either right now, we could potentially use it to error check for drops
+            stall = 1;
+        end
+        SendData: begin
+            phy_txen = 1'b1;
+            phy_txd = axiid; //coming from reverse_bit_order
+            stall = 0;
+        end
+        SendTail: begin
+            phy_txen = 1'b1;
+            if (dibit_counter <= 3) begin 
+                phy_txd = {cksum[25 + byte_bit_counter], cksum[24 + byte_bit_counter]};
+            end else if (dibit_counter <= 7) begin 
+                phy_txd = {cksum[17 + byte_bit_counter], cksum[16 + byte_bit_counter]};
+            end else if (dibit_counter <= 11) begin 
+                phy_txd = {cksum[9 + byte_bit_counter], cksum[8 + byte_bit_counter]};
+            end else if (dibit_counter <= 15) begin
+                phy_txd = {cksum[1 + byte_bit_counter], cksum[byte_bit_counter]};
+            end
+        end
+    endcase
+end
 
 // FSM below
 always_ff @(posedge clk) begin
     if (rst) begin
         state <= Idle;
-        stall <= 1'b1;
         byte_bit_counter <= 3'b0;
         audio_counter <= 9'b0;
-        phy_txen <= 1'b0;
-        phy_txd <= 2'b0;
-        cksum_axiiv <= 0;
+        crc32rst <= 1;
         dibit_counter <= 0;
-    end else if begin
+    end else begin
         case(state)
             Idle: begin //
-                stall <= 1;
                 byte_bit_counter <= 4'b0;
                 audio_counter <= 9'b0;
-                phy_txen <= 1'b0;
-                phy_txd <= 2'b0;
-                cksum_axiiv <= 1'b0;
+                crc32rst <= 0;
                 
-                if (dibit_counter < IFG_PERIOD) dibit_counter <= dibit_counter + 1;
-                else begin
+                if (dibit_counter < IFG_PERIOD) begin
+                    dibit_counter <= dibit_counter + 1;
+                end else begin
                     dibit_counter <= 0;
-                    state <= SendHead;
+                    state <= SendPre;
+                    $display("Sending Pre Now");
                 end
             end
             SendPre: begin
-                phy_txen <= 1;
-                stall <= 1;
-                cksum_axiiv <= 0;
                 if (dibit_counter < PREAMBLE_DIBITS) begin
                     dibit_counter <= dibit_counter + 1;
-                    phy_txd <= 2'b01;
                 end
                 else if (dibit_counter == PREAMBLE_DIBITS) begin
-                    phy_txd <= 2'b11;
                     dibit_counter <= 0;
                     state <= SendDestAddr;
+                    $display("Sending Dest Now");
                 end
             end
             SendDestAddr: begin
-                phy_txen <= 1;
-                stall <= 1;
-                cksum_axiiv <= 1;
+                crc32rst <= 0;
                 if (dibit_counter < ADDR_DIBITS) begin
                     dibit_counter <= dibit_counter + 1;
-                    phy_txd <= 2'b11;
-                end
-                else begin
+                end else begin
                     dibit_counter <= 0;
                     state <= SendSourceAddr;
+                    $display("Sending Source Now");
                 end
             end
             SendSourceAddr: begin //note: lowkey, we don't look at the source_addr in the existing code, this is kind of a waste to actually format rn
-                phy_txen <= 1;
-                cksum_axiiv <= 1;
+                crc32rst <= 0;
                 if (dibit_counter < ADDR_DIBITS) begin
                     dibit_counter <= dibit_counter + 1;
-                    phy_txd <= 2'b00;        
+                end else begin
+                    dibit_counter <= 0;
+                    state <= SendLength;
+                    $display("Sending Length Now");
+                end
+            end
+            SendLength: begin //question: do I actually want to send anything here? I don't use this field
+                crc32rst <= 0;
+                if (dibit_counter < LEN_DIBITS) begin
+                    dibit_counter <= dibit_counter + 1;
                 end else begin
                     dibit_counter <= 0;
                     state <= SendData;
-                    stall <= 0;
+                    $display("Sending Data Now");
                 end
             end
             SendData: begin
-                phy_txen <= 1;
-                cksum_axiiv <= 1;
-                stall <= 0;
+                crc32rst <= 0;
                 if (dibit_counter < MIN_DATA_DIBITS) begin
-                    phy_txd <= axiid; //question: i sent out an axiiv signal from reverse_bit_order, what should I do with that?
+                    dibit_counter <= dibit_counter + 1;
                 end else begin
-                    phy_txd <= axiid;
                     dibit_counter <= 0;
                     state <= SendTail;
+                    $display("Sending Tail Now");
                     byte_bit_counter <= 0;
                 end
             end
             SendTail: begin
-                phy_txen <= 1;
-                cksum_axiiv <= 0;
-                stall <= 1;
-                if (dibit_counter <= 6) begin 
-                    phy_txd <= {cksum[25 + byte_bit_counter], cksum[24 + byte_bit_counter]};
-                end else if (dibit_counter <= 14) begin 
-                    phy_txd <= {cksum[17 + byte_bit_counter], cksum[16 + byte_bit_counter]};
-                end else if (dibit_counter <= 22) begin 
-                    phy_txd <= {cksum[9 + byte_bit_counter], cksum[8 + byte_bit_counter]};
-                end else if (dibit_counter <= 30) begin
-                    phy_txd <= {cksum[1 + byte_bit_counter], cksum[byte_bit_counter]};
-                end
-
                 if (byte_bit_counter == 6) byte_bit_counter <= 0;
                 else byte_bit_counter <= byte_bit_counter + 2;
 
-                if (dibit_counter < 30) dibit_counter <= dibit_counter + 1;
-                else begin
+                if (dibit_counter < CRC_DIBITS) begin
+                    crc32rst <= 0;
+                    dibit_counter <= dibit_counter + 1;
+                end else begin
                     dibit_counter <= 0;
                     state <= Idle;
+                    crc32rst <= 1;
                 end
 
             end
-            default:
         endcase
     end
 end
