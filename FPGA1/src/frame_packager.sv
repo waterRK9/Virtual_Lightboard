7943cf9eb3e4b0066ed5aca47c27181602466fd0
Ivy Liu <96558257+waterRK9@users.noreply.github.com>
2022-11-15 18:35:16 -0500

    frame_packacger draft

--- FPGA1/src/frame_packager.sv
+++ FPGA1/src/frame_packager.sv
@@ -4,7 +4,7 @@
 module frame_packager(
     input wire clk,
     input wire rst,
-    input wire addr_axiov,
+    input wire addr_axiiv,
     input wire [23:0] addr_axiid,
     input wire pixel_axiiv,
     input wire [7:0] pixel_axiid,
@@ -14,10 +14,21 @@ module frame_packager(
     output logic [7:0] pixel_axiod
 );
 
-// pass pixel_axiiv right into pixel_axiod
-// only increment stored addr value when pixel_axiov high
-// don't want to increment addr value for first pixel, either that or subtract one from addr value when we recieve it?
-// 
+always_ff @(posedge clk) begin
+    if (rst) begin
+        axiov <= 0;
+        addr_axiod <= 0;
+        pixel_axiod <= 0;
+    end else begin
+        if (addr_axiiv) addr_axiod <= addr_axiid - 1; //to offset the +1 for when first pixel is sent in
+        else addr_axiod <= addr_axiod + 1;
+
+        if (pixel_axiiv) begin
+            pixel_axiod <= pixel_axiid;
+            axiov <= 1'b1;
+        end else axiov <= 1'b0;
+    end
+end
 
 endmodule
 
