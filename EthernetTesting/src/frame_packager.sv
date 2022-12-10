fd3cd9b77568ed09fd63292c0b52c59f265748a1
Ivy Liu <96558257+waterRK9@users.noreply.github.com>
2022-12-10 14:39:20 -0500

    fixing image_audio_splitter

---
+++ EthernetTesting/src/frame_packager.sv
@@ -0,0 +1,37 @@
+`default_nettype none
+`timescale 1ns / 1ps
+
+module frame_packager(
+    input wire clk,
+    input wire rst,
+    input wire addr_axiiv,
+    input wire [23:0] addr_axiid,
+    input wire pixel_axiiv,
+    input wire [7:0] pixel_axiid,
+
+    output logic axiov, //for wea on BRAM
+    output logic [16:0] addr_axiod,
+    output logic [7:0] pixel_axiod
+);
+
+// note: might add an always comb block and do everything but the count up combinationally
+
+always_ff @(posedge clk) begin
+    if (rst) begin
+        axiov <= 0;
+        addr_axiod <= 0;
+        pixel_axiod <= 0;
+    end else begin
+        if (addr_axiiv) addr_axiod <= addr_axiid[16:0]; //trunate from 24bit transmittion to 17bit BRAM address
+        else if (pixel_axiiv) addr_axiod <= addr_axiod + 1;
+
+        if (pixel_axiiv) begin
+            pixel_axiod <= pixel_axiid;
+            axiov <= 1'b1;
+        end else axiov <= 1'b0;
+    end
+end
+
+endmodule
+
+`default_nettype wire
